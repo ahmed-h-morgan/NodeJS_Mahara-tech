675d4e2652fbf3f66d3ce8c2334d6f3d954b10bd
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-10-27 06:45:41 +0300

    create hello.js

---
+++ demo/coreModules.js
@@ -0,0 +1,12 @@
+const EventEmitter = require("event")
+const myEventObj = new EventEmitter()
+// myEventObj.on("eventName", function)    - add a listener to event eventName using on and a function to be executed when this event fired which is 
+//      called event handler
+// myEventObj.emit("eventName")     - fire / trigger the event by calling emit with the eventName
+// we can add more than one en
+
+myEventObj.on("lookup", () => {
+    console.log("lookup event fired")
+});
+
+myEventObj.emit("lookup")
